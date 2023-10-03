a7c8ae728cee459e09558684178312916b7b1e47
Dominik Ježík <dominikjezik303@gmail.com>
2023-10-03 12:33:02 +0200

    Created initial ASP.NET Web App

---
+++ FirstResponder.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace FirstResponder.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
