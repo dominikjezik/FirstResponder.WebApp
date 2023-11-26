60f8423c5e7a05e7022acd8e781e1260061029e6
Dominik Ježík <dominikjezik303@gmail.com>
2023-11-26 15:53:43 +0100

    Added display labels for Aed State with colors.

--- FirstResponder.ApplicationCore/Enums/AedState.cs
+++ FirstResponder.ApplicationCore/Enums/AedState.cs
@@ -1,11 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace FirstResponder.ApplicationCore.Enums;
 
 public enum AedState
 {
+    [Display(Name="Registrovaný")]
     Registered,
+    
+    [Display(Name="Pripravený")]
     Ready,
+    
+    [Display(Name = "Nepripravený")]
     NotReady,
+    
+    [Display(Name = "Mimo prevádzky")]
     OutOfService,
+    
+    [Display(Name = "Servisný zásah")]
     ServiceRequired,
+    
+    [Display(Name = "Zrušený")]
     Cancelled
 }
