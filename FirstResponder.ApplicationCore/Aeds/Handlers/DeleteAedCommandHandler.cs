49ae6d2c489b3c33a6fe98a87e4372917e755d8c
Dominik Ježík <dominikjezik303@gmail.com>
2023-12-10 21:24:17 +0100

    When Aed is deleted, photos are also deleted.

--- FirstResponder.ApplicationCore/Aeds/Handlers/DeleteAedCommandHandler.cs
+++ FirstResponder.ApplicationCore/Aeds/Handlers/DeleteAedCommandHandler.cs
@@ -1,5 +1,6 @@
 ﻿using FirstResponder.ApplicationCore.Abstractions;
 using FirstResponder.ApplicationCore.Aeds.Commands;
+using FirstResponder.ApplicationCore.Entities.AedAggregate;
 using FirstResponder.ApplicationCore.Exceptions;
 using MediatR;
 
@@ -8,10 +9,12 @@ namespace FirstResponder.ApplicationCore.Aeds.Handlers;
 public class DeleteAedCommandHandler : IRequestHandler<DeleteAedCommand>
 {
     private readonly IAedRepository _aedRepository;
+    private readonly IFileService _fileService;
 
-    public DeleteAedCommandHandler(IAedRepository aedRepository)
+    public DeleteAedCommandHandler(IAedRepository aedRepository, IFileService fileService)
     {
         _aedRepository = aedRepository;
+        _fileService = fileService;
     }
 
     public async Task Handle(DeleteAedCommand request, CancellationToken cancellationToken)
@@ -29,7 +32,16 @@ public class DeleteAedCommandHandler : IRequestHandler<DeleteAedCommand>
         {
             throw new EntityNotFoundException();
         }
-
+        
+        if (aed is PublicAed publicAed)
+        {
+            var aedPhotos = await _aedRepository.GetAedPhotos(publicAed.Id);
+            foreach (var aedPhoto in aedPhotos)
+            {
+                await _fileService.DeleteFile(aedPhoto.PhotoName);
+            }
+        }
+        
         await _aedRepository.DeleteAed(aed);
     }
 }
